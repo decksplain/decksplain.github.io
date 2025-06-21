372f30b03145bc525c786c8ea5d3397c81798574
harvzor <harveyjwilliams@aol.co.uk>
2025-06-21 23:28:11 +0000

    Added a QR code which links to the page of the card.

---
+++ _plugins/qr.rb
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: GPL-3.0-only
+# SPDX-FileCopyrightText: 2024 Sebastian Crane <seabass-labrax@gmx.com>
+
+# frozen_string_literal: true
+
+require 'erb'
+require 'jekyll'
+require 'rqrcode'
+
+# Modified from https://codeberg.org/seabass/jekyll-qr/src/branch/dev/lib/jekyll-qr.rb to work with variables.
+# Main tag definition for Jekyll Liquid
+class QR < Liquid::Tag
+  RQRCODE_OPTIONS = {
+    level: :l
+  }.freeze
+
+  RQRCODE_SVG_OPTIONS = {
+    viewbox: true,
+    # offset is rqrcode's default module size of 11px multiplied by
+    # the recommended 'quiet area' for a QR code of four modules
+    use_path: true
+  }.freeze
+
+  def initialize(tag_name, text, tokens)
+    super
+    @text = text
+  end
+
+  def render(context)
+    resolved_text = Liquid::Template.parse(@text.strip).render(context)
+    qr = RQRCode::QRCode.new(resolved_text, **RQRCODE_OPTIONS)
+    qr_svg = ERB::Util.url_encode(qr.as_svg(**RQRCODE_SVG_OPTIONS).to_s)
+    "<img class=\"qr-code\" src=\"data:image/svg+xml;utf8,#{qr_svg}\">"
+  end
+
+  Liquid::Template.register_tag 'qr', self
+end
